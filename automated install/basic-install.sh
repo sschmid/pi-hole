d96201deccc600b37553c2ce666e8bbe54140216
Christian König <ckoenig@posteo.de>
2023-02-16 20:06:41 +0100

    Create dedicated getVal function in utils.sh as it might be useful somewhere else
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -2612,8 +2612,8 @@ main() {
 
         # Get the privacy level if it exists (default is 0)
         if [[ -f "${FTL_CONFIG_FILE}" ]]; then
-            # remove all lines with comments and extract the first PRIVACYLEVEL found
-            PRIVACY_LEVEL=$(sed -e '/^[[:blank:]]*#/d' "${FTL_CONFIG_FILE}" | awk -F 'PRIVACYLEVEL=' 'NR==1{printf$2}')
+            # use getVal from utils.sh to get PRIVACYLEVEL
+            PRIVACY_LEVEL=$(getVal "${FTL_CONFIG_FILE}" "PRIVACYLEVEL")
 
             # If no setting was found, default to 0
             PRIVACY_LEVEL="${PRIVACY_LEVEL:-0}"
